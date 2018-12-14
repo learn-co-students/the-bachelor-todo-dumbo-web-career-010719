fb8658dae59fe6814fda91bd5ea66bf5aad03308
github username <github email address>
2018-12-14 16:39:44 -0500

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,18 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+require 'json'
+def get_first_name_of_season_winner(data, season) # takes two arguments, hash called `data`, and season. returns *first name* of season's winner
+	data.each do |series_num, contestant_arrays|
+		if season = series_num
+			contestant_arrays.each do |contestant_hash|
+				contestant_hash.each do |attributes, info|
+					binding.pry
+					if contestant_hash["status"] = "Winner"
+						return contestant_hash['name'].split[0]					
+					end
+				end
+			end
+		end
+	end
 end
 
 def get_contestant_name(data, occupation)
