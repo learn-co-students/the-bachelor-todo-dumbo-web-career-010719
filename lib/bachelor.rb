282a2aa910628a28d579e096fd35b405db023749
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-16 02:15:18 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,55 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  win = ""
+  data[season].each {|elemObj|
+    if elemObj["status"] == "Winner"
+      win = elemObj["name"]
+      return win.split(" ")[0]
+    end
+  }
+
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each {|season, elemObjArr|
+    #win = elemObjArr[0]["occupation"]
+    elemObjArr.each{|personObj|
+      if personObj["occupation"] == occupation
+        return personObj["name"]
+      end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each {|season, elemObjArr|
+    #win = elemObjArr[0]["occupation"]
+    elemObjArr.each{|personObj|
+      if personObj["hometown"] == hometown
+        counter += 1
+      end
+    }
+  }
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each {|season, elemObjArr|
+    #win = elemObjArr[0]["occupation"]
+    elemObjArr.each{|personObj|
+      if personObj["hometown"] == hometown
+        return personObj["occupation"]
+      end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  counter = 0
+  ageTotal = 0
+  data[season].each {|elemObj|
+    ageTotal += elemObj["age"].to_f
+    counter += 1
+  }
+  (ageTotal/counter).round
 end
